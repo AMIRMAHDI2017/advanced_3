369b02cbf4f24ad71fe983a14df91022c888246c
AMIRMAHDI2017 <150344358+AMIRMAHDI2017@users.noreply.github.com>
2024-01-04 14:53:11 +0330

    Update ربات اینستاگرام

---
+++ ماشین حساب/SQL3.py/ربات اینستاگرام/robat2.py
@@ -0,0 +1,55 @@
+
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QTextEdit
+from instagrapi import Client
+from instagrapi.types import MediaTypes
+
+class InstagramBot(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+        
+        self.username = 'your_username'
+        self.password = 'your_password'
+        self.api = Client()
+        
+    def init_ui(self):
+        self.setWindowTitle('Instagram Bot')
+        
+        layout = QVBoxLayout()
+        
+        self.like_button = QPushButton('Like', self)
+        self.like_button.clicked.connect(self.like_photos)
+        
+        self.messages_label = QLabel('Messages:')
+        
+        self.messages_text = QTextEdit(self)
+        self.messages_text.setReadOnly(True)
+        
+        layout.addWidget(self.like_button)
+        layout.addWidget(self.messages_label)
+        layout.addWidget(self.messages_text)
+        
+        self.setLayout(layout)
+        
+    def login(self):
+        self.api.login(self.username, self.password)
+        
+    def like_photos(self):
+        self.login()
+        media = self.api.get_user_feed(self.api.user_id)
+        for m in media:
+            if m.like_count < 10:  # لایک فقط برای عکس هایی که کمتر از 10 لایک دارند
+                self.api.like(m.pk, MediaTypes.PHOTO)
+        
+    def get_messages(self):
+        self.login()
+        messages = self.api.get_inbox_v2()
+        for thread in messages.threads:
+            for message in thread.items:
+                self.messages_text.append(message.text)
+        
+if __name__ == '__main__':
+    app = QApplication([])
+    window = InstagramBot()
+    window.show()
+    app.exec_()
